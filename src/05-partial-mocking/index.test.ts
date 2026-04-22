c9a00547941244511942cd83e5629c310665831e
Natallia <natasyo23@gmail.com>
2026-04-22 12:54:07 +0300

    feat: test 5

--- src/05-partial-mocking/index.test.ts
+++ src/05-partial-mocking/index.test.ts
@@ -1,20 +1,40 @@
 // Uncomment the code below and write your tests
 // import { mockOne, mockTwo, mockThree, unmockedFunction } from './index';
 
+import { mockOne, mockThree, mockTwo, unmockedFunction } from './index';
+
 jest.mock('./index', () => {
-  // const originalModule = jest.requireActual<typeof import('./index')>('./index');
+  const originalModule =
+    jest.requireActual<typeof import('./index')>('./index');
+  return {
+    __esModule: true,
+
+    ...originalModule,
+    mockOne: jest.fn(),
+    mockThree: jest.fn(),
+    mockTwo: jest.fn(),
+  };
 });
 
 describe('partial mocking', () => {
   afterAll(() => {
     jest.unmock('./index');
   });
-
+  let consoleSpy: jest.SpyInstance;
+  beforeEach(() => {
+    consoleSpy = jest.spyOn(console, 'log').mockImplementation(() => {});
+  });
   test('mockOne, mockTwo, mockThree should not log into console', () => {
     // Write your test here
+    mockOne();
+    mockTwo();
+    mockThree();
+    expect(consoleSpy).not.toHaveBeenCalled();
   });
 
   test('unmockedFunction should log into console', () => {
     // Write your test here
+    unmockedFunction();
+    expect(consoleSpy).toHaveBeenCalled();
   });
 });
